6827237d8b3e9b35abb22cfa77bb4a7497c6b216
SarahDomscheit <sarri91@googlemail.com>
2025-12-16 14:57:06 +0100

    feat: add CustomersService provider to CustomersController tests

--- backend/src/customers/customers.controller.spec.ts
+++ backend/src/customers/customers.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
 
 describe('CustomersController', () => {
   let controller: CustomersController;
@@ -7,6 +8,21 @@ describe('CustomersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CustomersController],
+      providers: [
+        {
+          provide: CustomersService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOneCustomer: jest.fn(),
+            findCustomerByEmail: jest.fn(),
+            findAllForUser: jest.fn(),
+            update: jest.fn(),
+            updateForUser: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<CustomersController>(CustomersController);
